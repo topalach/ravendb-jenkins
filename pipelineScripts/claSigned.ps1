d95c01d0eec8fcb8075809c39ab7a3b49877463a
Tomasz Opalach <tomasz.opalach@gmail.com>
2018-01-09 15:27:32 +0100

    fix

--- pipelineScripts/claSigned.ps1
+++ pipelineScripts/claSigned.ps1
@@ -3,6 +3,7 @@ $ErrorActionPreference = "Stop"
 $url = $env:CLA_SIGNED_URL + $env:ghprbPullId
 Invoke-RestMethod -Method Get -Uri $url -ContentType "application/json" -Body $json
 
+Write-Host '[LOG] URL: ' $url
 Write-Host '[LOG] Body: ' $json
 
 if ($json -contains "Not all commiters signed CLA")
