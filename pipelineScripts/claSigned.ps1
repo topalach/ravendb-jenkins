f3e3269b8ebce433deb03cc2522d378e8293eb5f
Tomasz Opalach <tomasz.opalach@gmail.com>
2018-01-10 09:08:52 +0100

    log invoke-restmethod

--- pipelineScripts/claSigned.ps1
+++ pipelineScripts/claSigned.ps1
@@ -1,6 +1,13 @@
 $ErrorActionPreference = "Stop"
 
 $url = $env:CLA_SIGNED_URL + $env:ghprbPullId
-Invoke-RestMethod -Method Get -Uri $url -ContentType "application/json"
+
+try {
+    $response = Invoke-RestMethod -Method Get -Uri $url -ContentType "application/json" -UseBasicParsing
+} catch {
+    Write-Host "[LOG] StatusCode:" $_.Exception.Response.StatusCode.value__ 
+    Write-Host "[LOG] StatusDescription:" $_.Exception.Response.StatusDescription
+}
 
 Write-Host '[LOG] URL: ' $url
+Write-Host '[LOG] Response: ' $response
