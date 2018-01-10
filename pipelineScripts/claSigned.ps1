b234f58f9360d5f69596f75db4050c201ca17f0f
Tomasz Opalach <tomasz.opalach@gmail.com>
2018-01-10 09:14:39 +0100

    fix

--- pipelineScripts/claSigned.ps1
+++ pipelineScripts/claSigned.ps1
@@ -1,13 +1,7 @@
 $ErrorActionPreference = "Stop"
 
 $url = $env:CLA_SIGNED_URL + $env:ghprbPullId
-
-try {
-    $response = Invoke-RestMethod -Method Get -Uri $url -ContentType "application/json" -UseBasicParsing
-} catch {
-    Write-Host "[LOG] StatusCode:" $_.Exception.Response.StatusCode.value__ 
-    Write-Host "[LOG] StatusDescription:" $_.Exception.Response.StatusDescription
-}
+$response = Invoke-RestMethod -Method Get -Uri $url -ContentType "application/json" -UseBasicParsing
 
 Write-Host '[LOG] URL: ' $url
 Write-Host '[LOG] Response: ' $response
