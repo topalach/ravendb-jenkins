4e9fd44b3a69003168916376b9b9963800984ec6
Tomasz Opalach <tomtom@ravendb.net>
2018-01-31 08:28:51 +0100

    fix

--- Shared_Scripts/v4.0/start_server.ps1
+++ Shared_Scripts/v4.0/start_server.ps1
@@ -1,3 +1,4 @@
 param($ServerUrl="")
 
-Start-Process -FilePath ".\Raven.Server.exe" -ArgumentList "--ServerUrl=$ServerUrl --RunInMemory=true --Non-interactive" -NoNewWindow
+# Start-Process -FilePath ".\Raven.Server.exe" -ArgumentList "--ServerUrl=$ServerUrl --RunInMemory=true --Non-interactive" -NoNewWindow
+Start-Process -FilePath ".\Raven.Server.exe" -ArgumentList "--ServerUrl=$ServerUrl --RunInMemory=true" -NoNewWindow
